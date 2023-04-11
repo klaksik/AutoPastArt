dcfd4ecbecf41cd26bc0bd3a32c85796a73453ed
klaksik <89664351+klaksik@users.noreply.github.com>
2023-04-11 15:58:46 +0300

    Update create_pack.py

--- create_pack.py
+++ create_pack.py
@@ -3,12 +3,12 @@ import telebot
 from send_pack import send
 from time import *
 
+
 def create_pack(one_buffer, one_pack, bot, channel_id):
-    for i in range (one_pack):
+    for i in range(one_pack):
         photos = createbuff(one_buffer)
         print(photos)
         media = [telebot.types.InputMediaPhoto(open(photo, 'rb').read()) for photo in photos]
         print(media)
         send(media, bot, channel_id, photos)
-    sleep(15)
-
+        sleep(15)
